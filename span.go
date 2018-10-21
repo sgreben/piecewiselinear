8144b95e616ba79ffcaf2ebb7018695ac8a169e8
Sergey Grebenshchikov <sgreben@gmail.com>
2018-10-21 15:28:57 +0200

    Fix span

--- span.go
+++ span.go
@@ -5,8 +5,8 @@ import "math"
 // Span generates `nPoints` equidistant points spanning [min,max]
 func Span(min, max float64, nPoints int) []float64 {
 	X := make([]float64, nPoints)
-	d := max - min
 	min, max = math.Min(max, min), math.Max(max, min)
+	d := max - min
 	for i := range X {
 		X[i] = min + d*(float64(i)/float64(nPoints-1))
 	}
